a4b7fa12d41909477daf88db81388fe0d4016a4d
Laszlo_Stahorszki <laszlo_stahorszki@epam.com>
2022-11-19 13:29:35 +0100

    add h2 db

--- src/main/java/com/hackyeah/hy22/repositories/DummyRepo.java
+++ src/main/java/com/hackyeah/hy22/repositories/DummyRepo.java
@@ -1,6 +1,7 @@
 package com.hackyeah.hy22.repositories;
 
+import com.hackyeah.hy22.models.FoodProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DummyRepo extends JpaRepository {
+public interface DummyRepo extends JpaRepository<FoodProduct, Long> {
 }
