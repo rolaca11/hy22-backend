bb9a82e16150c848023310c4d750adfa1a85138b
Nurary <hegyibalint.08@gmail.com>
2022-11-19 13:27:57 +0100

    Id addon

--- src/main/java/com/hackyeah/hy22/models/FoodProduct.java
+++ src/main/java/com/hackyeah/hy22/models/FoodProduct.java
@@ -2,16 +2,26 @@ package com.hackyeah.hy22.models;
 
 import java.time.ZonedDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class FoodProduct {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String productName;
     private ZonedDateTime insertDate;
 }
