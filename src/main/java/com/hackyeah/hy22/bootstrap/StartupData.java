7111605b91f8d11e4adcfd92d2a75fef9e0ccbb5
Nurary <hegyibalint.08@gmail.com>
2022-11-19 13:45:51 +0100

    dumy DB implementation

--- src/main/java/com/hackyeah/hy22/bootstrap/StartupData.java
+++ src/main/java/com/hackyeah/hy22/bootstrap/StartupData.java
@@ -1,5 +1,8 @@
 package com.hackyeah.hy22.bootstrap;
 
+import java.time.ZonedDateTime;
+
+import com.hackyeah.hy22.models.FoodProduct;
 import com.hackyeah.hy22.repositories.DummyRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +16,25 @@ public class StartupData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        dummyRepo.save(FoodProduct.builder().productName("Milk")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Cheese")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Banana")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Sour Cream")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Raspberry")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Crack")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Chicken")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Fish")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Bread")
+            .insertDate(ZonedDateTime.now()).build());
+        dummyRepo.save(FoodProduct.builder().productName("Butter")
+            .insertDate(ZonedDateTime.now()).build());
     }
 }
